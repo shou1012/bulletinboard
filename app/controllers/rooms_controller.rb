3afa94084593c62089883d41e27f5d2805353e11
shohan ki <sho1012.seikyo@gmail.com>
2017-04-04 14:41:22 +0900

    fb fixed2

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,11 @@
 class RoomsController < ApplicationController
   def index
     @rooms = Room.all
-    respond_to do |format|
-    format.json { render json: @rooms } # URLが.jsonの場合、@products.to_json が返される
-    end
+    render 'index', formats: 'json', handlers: 'jbuilder'
   end
 
   def show
     @room = Room.find(params[:id])
     render 'show', formats: 'json', handlers: 'jbuilder'
    end
-  end
+end
--- app/views/rooms/index.json.jbuilder
+++ app/views/rooms/index.json.jbuilder
@@ -1,5 +1,3 @@
-
-json.array!(@rooms) do |room|
-  json.extract! product, :id, :name, :user_id, :description, :created_at, :updated_at,
-  json.url room_url(room, format: :json)
+json.data do |json|
+  json.all @rooms
 end
