f06b623aad7f2a19ed9ae89154f4729a53ab24d9
shohan ki <sho1012.seikyo@gmail.com>
2017-04-04 19:12:39 +0900

    rooms create api added

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,6 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:update]
+
   def index
     @rooms = Room.all
     render 'index', formats: 'json', handlers: 'jbuilder'
@@ -11,18 +13,41 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.new(room_params)
-    p room_params
-    p @room
-
-    if @room.save
-      respond_to do |format|
-      format.json { render json: @room, status: :created }
+    respond_to do |format|
+      if @room.save
+          format.json { render json: @room, status: :created }
+      else
+          format.json { render status: :unprocessable_entity, :json => { :errors => "Invalid Arguments" } }
       end
     end
   end
 
+  def update
+    if @room
+      respond_to do |format|
+        if @room.update(room_params)
+            format.json { render json: @room, status: :updated }
+        else
+            format.json { render status: :unprocessable_entity, :json => { :errors => "Invalid Arguments" } }
+        end
+      end
+    end
+  end
+
+
   private
     def room_params
-      params.permit(:name, :user_id, :description)
+      params.require(:room).permit(:name, :user_id, :description)
     end
+
+    def set_room
+      begin
+        @room = Room.find(params[:id])
+      rescue ActiveRecord::RecordNotFound => e
+        respond_to do |format|
+          format.json { render status: :not_found, :json => { :errors => "Data not found" } }
+        end
+      end
+    end
+
 end
--- test/controllers/test_controller.rb
+++
@@ -1,7 +0,0 @@
-class TestController < ApplicationController
-  protect_from_forgery :except => [:sample]
-
-  def sample
-    ## 処理
-  end
-end
