2533e4981e54e7c1b4a436ef4392a665edde688c
shohan ki <sho1012.seikyo@gmail.com>
2017-04-04 17:19:40 +0900

    testing rooms create api

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,4 +8,20 @@ class RoomsController < ApplicationController
     @room = Room.find(params[:id])
     render 'show', formats: 'json', handlers: 'jbuilder'
    end
+
+  def create
+    logger.debug(params)
+    @room = Room.new()
+
+    if @room.save(room_params)
+      respond_to do |format|
+      format.json { render json: @room, status: :created }
+      end
+    end
+  end
+
+  private
+    def room_params
+      params.permit(:name, :user_id, :description)
+  end
 end
