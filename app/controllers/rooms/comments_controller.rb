90fe8354129925b0f3ddd3ad0ea76cdd72eab1c7
shohan ki <sho1012.seikyo@gmail.com>
2017-04-07 02:21:36 +0900

    comments create api added

--- app/controllers/rooms/comments_controller.rb
+++ app/controllers/rooms/comments_controller.rb
@@ -6,8 +6,23 @@ class Rooms::CommentsController < ApplicationController
     render 'index', formats: 'json', handlers: 'jbuilder'
   end
 
+  def create
+    @comment = Comment.new(comment_params)
+    respond_to do |format|
+      if @comment.save
+        format.json { render json: @comment, status: :created }
+      else
+        format.json { render status: :unprocessable_entity, json: { errors: "Invalid Arguments" }}
+      end
+    end
+  end
+
   private
     def set_room
       @room = Room.find(params[:room_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:text, :user_id, :room_id)
     end
 end
