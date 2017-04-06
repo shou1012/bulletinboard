cff681e88ce4d7d891c1f9630eb67bc1f3112504
shohan ki <sho1012.seikyo@gmail.com>
2017-04-06 15:28:24 +0900

    indents fixed

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: [:update]
+  before_action :set_room, only: [:update, :destroy]
 
   def index
     @rooms = Room.all
@@ -15,9 +15,9 @@ class RoomsController < ApplicationController
     @room = Room.new(room_params)
     respond_to do |format|
       if @room.save
-          format.json { render json: @room, status: :created }
+        format.json { render json: @room, status: :created }
       else
-          format.json { render status: :unprocessable_entity, :json => { :errors => "Invalid Arguments" } }
+        format.json { render status: :unprocessable_entity, json: { errors: "Invalid Arguments" } }
       end
     end
   end
@@ -26,14 +26,25 @@ class RoomsController < ApplicationController
     if @room
       respond_to do |format|
         if @room.update(room_params)
-            format.json { render json: @room, status: :updated }
+          format.json { render json: @room, status: :updated }
         else
-            format.json { render status: :unprocessable_entity, :json => { :errors => "Invalid Arguments" } }
+          format.json { render status: :unprocessable_entity, json: { errors: "Invalid Arguments" } }
         end
       end
     end
   end
 
+  def destroy
+    if @room
+      respond_to do |format|
+        if @room.destroy
+          format.json { head :no_content }
+        else
+          format.json { render status: :unprocessable_entity, json:{ errors: "Invalid Arguments" } }
+        end
+      end
+    end
+  end
 
   private
     def room_params
@@ -45,7 +56,7 @@ class RoomsController < ApplicationController
         @room = Room.find(params[:id])
       rescue ActiveRecord::RecordNotFound => e
         respond_to do |format|
-          format.json { render status: :not_found, :json => { :errors => "Data not found" } }
+          format.json { render status: :not_found, json:{ errors: "Data not found" } }
         end
       end
     end
