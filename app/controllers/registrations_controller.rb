fe7a97acde906e5dbc3948d26990bc38f632aa61
shohan ki <sho1012.seikyo@gmail.com>
2017-04-17 21:48:14 +0900

    delete response fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -40,7 +40,15 @@ class RegistrationsController < Devise::RegistrationsController
       end
   end
 
-    private
+  def destroy
+    if @user.destroy
+      head :no_content
+    else
+      render json: { error: t('user_delete_error') }, status: :unprocessable_entity
+    end
+  end
+
+  private
     def user_params
       params.require(:user).permit(:email, :password)
     end
